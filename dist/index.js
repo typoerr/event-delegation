3db3c789a896c2f8edfc0cbffe9f2e423de4e55a
nullabletypo <masahirokato.dev@gmail.com>
2019-04-08 14:25:39 +0900

    0.0.2

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,67 @@
-var t=function(t,e){void 0===e&&(e={}),this.options={},this.target=t,this.options=e};t.prototype.on=function(t,e,n,r){var o=this;r=Object.assign({},this.options,r);var i=function(t,e,n){return function(r){var o,i=function(t,e,n){var r=t.parentElement;do{if(e.matches(n))return e;e=e.parentElement}while(null!=e&&e!==r);return null}(t,r.target,e);null!=i&&n.call(i,(o=i,new Proxy(r,{get:function(t,e){return"target"===e?o:Reflect.get(t,e)}})))}}(this.target,e,n);return this.target.addEventListener(t,i,r),function(){return o.target.removeEventListener(t,i,r)}},exports.delegate=function(e,n){return new t(e,n)},exports.fire=function(t,e,n){var r=new CustomEvent(e,{bubbles:!0,composed:!0,cancelable:!0,detail:n});return t.dispatchEvent(r)},exports.EventDelegator=t;
+var closest = function (root, el, sel) {
+  // include root node
+  var parent = root.parentElement;
+
+  do {
+    if (el.matches(sel)) { return el; }
+    el = el.parentElement;
+  } while (el != null && el !== parent);
+
+  return null;
+}; // Disguise event.target as matched element
+
+var proxy = function (ev, target) {
+  var get = function (event, prop) { return prop === 'target' ? target : Reflect.get(event, prop); };
+
+  return new Proxy(ev, {
+    get: get
+  });
+};
+var listener = function (root, sel, handler) { return function (e) {
+  var target = e.target;
+  var matched = closest(root, target, sel);
+
+  if (matched != null) {
+    handler.call(matched, proxy(e, matched));
+  }
+}; };
+var EventDelegator = function EventDelegator(target, options) {
+  if ( options === void 0 ) options = {};
+
+  this.options = {};
+  this.target = target;
+  this.options = options;
+};
+
+EventDelegator.prototype.on = function on (type, sel, handler, options) {
+    var this$1 = this;
+
+  options = Object.assign({}, this.options,
+    options);
+
+  var _listener = listener(this.target, sel, handler);
+
+  this.target.addEventListener(type, _listener, options);
+  return function () { return this$1.target.removeEventListener(type, _listener, options); };
+};
+var delegate = function (el, defaultOptions) {
+  return new EventDelegator(el, defaultOptions);
+};
+/**
+ * Shorthand of CustomEvent
+ */
+
+var fire = function (el, type, detail) {
+  var event = new CustomEvent(type, {
+    bubbles: true,
+    composed: true,
+    cancelable: true,
+    detail: detail
+  });
+  return el.dispatchEvent(event);
+};
+
+exports.delegate = delegate;
+exports.fire = fire;
+exports.EventDelegator = EventDelegator;
 //# sourceMappingURL=index.js.map
--- dist/index.mjs
+++ dist/index.mjs
@@ -1,2 +1,65 @@
-var t=function(t,n){void 0===n&&(n={}),this.options={},this.target=t,this.options=n};t.prototype.on=function(t,n,e,r){var i=this;r=Object.assign({},this.options,r);var o=function(t,n,e){return function(r){var i,o=function(t,n,e){var r=t.parentElement;do{if(n.matches(e))return n;n=n.parentElement}while(null!=n&&n!==r);return null}(t,r.target,n);null!=o&&e.call(o,(i=o,new Proxy(r,{get:function(t,n){return"target"===n?i:Reflect.get(t,n)}})))}}(this.target,n,e);return this.target.addEventListener(t,o,r),function(){return i.target.removeEventListener(t,o,r)}};var n=function(n,e){return new t(n,e)},e=function(t,n,e){var r=new CustomEvent(n,{bubbles:!0,composed:!0,cancelable:!0,detail:e});return t.dispatchEvent(r)};export{n as delegate,e as fire,t as EventDelegator};
+var closest = function (root, el, sel) {
+  // include root node
+  var parent = root.parentElement;
+
+  do {
+    if (el.matches(sel)) { return el; }
+    el = el.parentElement;
+  } while (el != null && el !== parent);
+
+  return null;
+}; // Disguise event.target as matched element
+
+var proxy = function (ev, target) {
+  var get = function (event, prop) { return prop === 'target' ? target : Reflect.get(event, prop); };
+
+  return new Proxy(ev, {
+    get: get
+  });
+};
+var listener = function (root, sel, handler) { return function (e) {
+  var target = e.target;
+  var matched = closest(root, target, sel);
+
+  if (matched != null) {
+    handler.call(matched, proxy(e, matched));
+  }
+}; };
+var EventDelegator = function EventDelegator(target, options) {
+  if ( options === void 0 ) options = {};
+
+  this.options = {};
+  this.target = target;
+  this.options = options;
+};
+
+EventDelegator.prototype.on = function on (type, sel, handler, options) {
+    var this$1 = this;
+
+  options = Object.assign({}, this.options,
+    options);
+
+  var _listener = listener(this.target, sel, handler);
+
+  this.target.addEventListener(type, _listener, options);
+  return function () { return this$1.target.removeEventListener(type, _listener, options); };
+};
+var delegate = function (el, defaultOptions) {
+  return new EventDelegator(el, defaultOptions);
+};
+/**
+ * Shorthand of CustomEvent
+ */
+
+var fire = function (el, type, detail) {
+  var event = new CustomEvent(type, {
+    bubbles: true,
+    composed: true,
+    cancelable: true,
+    detail: detail
+  });
+  return el.dispatchEvent(event);
+};
+
+export { delegate, fire, EventDelegator };
 //# sourceMappingURL=index.mjs.map

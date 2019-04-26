2116cfb80b72a7597d79188cf2bf12558394cfec
nullabletypo <masahirokato.dev@gmail.com>
2019-04-26 18:04:41 +0900

    add: rx-event-delegation

---
+++ rx/src/index.ts
@@ -0,0 +1,20 @@
+import { fromEventPattern, Observable } from 'rxjs'
+import { delegate as _delegate, EventDelegatorOptions as Opts, EventHandler } from 'event-delegation'
+
+type Type = keyof HTMLElementEventMap
+
+export function delegate(target: EventTarget, options?: Opts) {
+  const del = _delegate(target, options)
+  return on
+
+  function on<T extends Event>(type: Type, opts?: Opts): Observable<T>
+  function on<T extends Event>(type: string, opts?: Opts): Observable<T>
+  function on<T extends Event>(type: Type, sel: string, opts?: Opts): Observable<T>
+  function on<T extends Event>(type: string, sel: string, opts?: Opts): Observable<T>
+  function on<T extends Event>(type: string, a: any, b?: any) {
+    const [sel, opts] = typeof a === 'string' ? [a, b] : [undefined, b]
+    const add = (next: EventHandler) => del.on(type, sel, next, opts)
+    const remove = (next: EventHandler) => del.off(type, next, opts)
+    return fromEventPattern<T>(add, remove)
+  }
+}
---
+++ rx/test/index.test.ts
@@ -0,0 +1,37 @@
+import { delegate } from '../src'
+import { tap, map, take, toArray } from 'rxjs/operators'
+
+beforeAll(() => {
+  const html = String.raw
+  const template = document.createElement('template')
+  template.innerHTML = html`
+    <div class="a">
+      <div class="b">
+        <div class="c">child a</div>
+      </div>
+    </div>
+  `
+  document.body.appendChild(template.content)
+})
+
+test('delegate', done => {
+  const c = document.querySelector('.c') as HTMLElement
+  const on = delegate(document.body, { capture: true })
+  const mock = jest.fn((ev: Event) => ev)
+
+  const event$ = on('click', '.c').pipe(
+    map(mock),
+    tap(e => expect(e.target).toBe(c)),
+    take(1),
+    toArray(),
+  )
+
+  event$.toPromise().then(arr => {
+    expect(mock).toBeCalledTimes(1)
+    expect(arr.length).toBe(1)
+    done()
+  })
+
+  c.click()
+  c.click()
+})

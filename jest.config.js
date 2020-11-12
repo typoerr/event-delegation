37d375b8bed19f777c499c55d839dc656fb3aff8
typoerr <masahirokato.dev@gmail.com>
2020-11-12 16:09:25 +0900

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   globals: {
     'ts-jest': {
-      tsConfig: {
+      tsconfig: {
         baseUrl: '.',
         paths: {
           '@typoerr/event-delegation': ['./packages/core/index.ts'],
